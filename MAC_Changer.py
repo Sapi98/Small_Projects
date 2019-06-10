bbe4161d850de51197d68995feac89f65eec53e7
sapi <saptarshi.biswas9@gmail.com>
2019-06-10 21:55:40 +0530

    Class MACChanger is defined and all changes are done accordingly

--- MAC_Changer.py
+++ MAC_Changer.py
@@ -1,6 +1,53 @@
 import os
 import subprocess
 
+class MACChanger:
+    def __init__(self):
+        self.cmd = None
+        self.device = None
+        self.new_mac = None
+        self.old_mac = None
+        self.pswd = None
+
+    def process_exec(self, cmd, console_print=False, return_report=False):
+        proc = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        out, err = proc.communicate()
+        if err != None:
+            print(err)
+        elif console_print:
+            print(out)
+        if return_report:
+            return out.splitlines()
+
+    def set_command(self, command_string):
+        self.cmd = command_string.strip().split()
+    
+    def check_device(self, out):
+        index = -1
+        for i in range(len(out)):
+            exp = out[i]
+            if device in exp:
+                index = i+1
+                break
+        
+        if index == -1:
+            return False
+        else:
+            return True
+    
+    def check_mac(self):
+        l = len(self.new_mac)
+        if l != 6:
+            return False
+        else:
+            for i in self.new_mac:
+                if len(i) != 2:
+                    return False
+        return True
+    
+    def main(self):
+        pass
+
 cmd = ["ip", "link", "show"]
 
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -10,23 +57,8 @@ out = out.splitlines()
 
 device = input("Enter the name of the device (eg eth0) : ")
 new_mac = input("Enter the New MAC that you want to set (in form xx:xx:xx:xx:xx:xx): ").strip().split(':')
-l = len(new_mac)
+
 old_mac = None
-
-if l != 6:
-    print("Enter a valid MAC Address")
-else:
-    for i in new_mac:
-        if len(i) != 2:
-            print("Enter a valid MAC Address")
-            break
-
-index = -1
-for i in range(len(out)):
-    exp = out[i]
-    if device in exp:
-        index = i+1
-        break
 
 if index == -1:
     print("The device you have entered does not exists")
